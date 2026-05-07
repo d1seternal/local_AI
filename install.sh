8b9853e86ce5ac1147119c908aef8e3268211e80
d1seternal <220264454+d1seternal@users.noreply.github.com>
2026-05-07 16:29:51 +0300

    updated install.sh: python version

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ if [ ! -f /swapfile ]; then
 fi
 
 
-python3.11 -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 
 pip install --upgrade pip setuptools wheel
@@ -36,22 +36,3 @@ CMAKE_ARGS="-DLLAMA_BLAS=ON -DLLAMA_BLAS_VENDOR=OpenBLAS" \
 pip install llama-cpp-python --force-reinstall --no-cache-dir
 
 
-
-
-
-
-# # Python
-# sudo apt update
-# sudo apt install -y python3.11 python3-pip python3-venv
-# # CMake
-# sudo apt install -y cmake
-
-# # Создание виртуального окружения
-# python3.11 -m venv venv
-# source venv/bin/activate  # Linux/Mac
-
-# # Установка пакетов
-# pip install -r requirements.txt
-
-# # Установка llama-cpp-python с оптимизацией
-# CMAKE_ARGS="-DLLAMA_BLAS=ON -DLLAMA_BLAS_VENDOR=OpenBLAS" pip install llama-cpp-python --force-reinstall --no-cache-dir
